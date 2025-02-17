33b2922676a2121afbebbabfd092df22589555b8
yramoliya <yashramoliya58@gmail.com>
2025-02-17 11:33:32 +0530

    add the register class file

---
+++ Models/Register_user.cs
@@ -0,0 +1,25 @@
+﻿namespace Banking_Management_System_Major_Project.Models
+{
+    public class Register_user
+    {
+            public string? Title { get; set; }
+            public string? FirstName { get; set; }
+            public string? MiddleName { get; set; }
+            public string? LastName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string? Gender { get; set; }
+            public string? ParentName { get; set; }
+            public string? Nationality { get; set; }
+            public string? Address { get; set; }
+            public int ZipCode { get; set; }
+            public string? Area { get; set; }
+            public string? Country { get; set; }
+            public int PhoneCode { get; set; }
+            public int PhoneNumber { get; set; }
+            public string? Email { get; set; }
+            public bool TermsAccepted { get; set; }
+            public int AadhaarCardNumber { get; set; }
+            public int PanCardNumber { get; set; }
+        }
+
+    }
