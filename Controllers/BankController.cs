ee0960aa86b7226e20c2bdf4c67017279767a72c
yramoliya <yashramoliya58@gmail.com>
2025-02-17 09:50:29 +0530

    Add project files.

---
+++ Controllers/BankController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Banking_Management_System_Major_Project.Controllers
+{
+    public class BankController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Banking_Management_System_Major_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
