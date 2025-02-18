052033591d332027597848664451180d199e3c12
yramoliya <yashramoliya58@gmail.com>
2025-02-18 17:56:42 +0530

    add the more file

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Banking_Management_System_Major_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult User_Manage()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/BankController.cs
+++ Controllers/BankController.cs
@@ -14,5 +14,23 @@ namespace Banking_Management_System_Major_Project.Controllers
             return View();
         }
 
+        public IActionResult LoanService()
+        {
+            return View();
+        }
+        public IActionResult Transactions()
+        {
+            return View();
+        }
+
+        public IActionResult Fund() 
+        {
+            return View();
+        }
+
+        public IActionResult InvestmentPlan()
+        {
+            return View();
+        }
     }
 }
