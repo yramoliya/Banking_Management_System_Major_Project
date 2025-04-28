14cc445124c17c6d8738e875c6433e4e120c8016
Parth Tank <132151538+parthtank554@users.noreply.github.com>
2025-04-28 20:40:08 +0530

    Card, Accoun

--- Models/AdminModel/UserRegistration.cs
+++ Models/AdminModel/UserRegistration.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Net.Mail;
 using System.Net;
 using Microsoft.Data.SqlClient;
